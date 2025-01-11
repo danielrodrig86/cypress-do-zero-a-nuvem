42f22b46677a7d5ac3d943d7465d15783c6d8297
danielrodrig86 <daniel.rodrigues1998@live.com>
2025-01-10 21:16:15 -0300

    exercicios corrigidos

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -10,201 +10,106 @@ describe('Central de Atendimento ao Cliente TAT', () => {
 
   it('Exercício - preenche os campos obrigatórios e envia o formulário', () => {
 
-    cy.get('#firstName')
-    .as('firstName')
-    .should('be.visible')
-    .type('daniel')
+    cy.get('#firstName').as('firstName').should('be.visible').type('Daniel')
+    cy.get('#firstName').should('have.value', 'Daniel')
+    cy.get('#lastName').as('lastName').should('be.visible').type('Rodrigues')
+    cy.get('#lastName').should('have.value', 'Rodrigues')
+    cy.get('#email').as('email').should('be.visible').type('daniel@teste.com.br')
+    cy.get('#email').should('have.value', 'daniel@teste.com.br')
+    cy.get('#open-text-area').as('open-text-area').should('be.visible').type('Obrigado')
+    cy.get('#open-text-area').should('have.value', 'Obrigado')
+    cy.get('button[type="submit"]').click()
 
-    cy.get('#firstName')
-    .should('have.value', 'daniel')
-
-    cy.get('#lastName')
-    .as('lastName')
-    .should('be.visible')
-    .type('rodrigues')
-
-    cy.get('#lastName')
-    .should('have.value', 'rodrigues')
-
-    cy.get('#email')
-    .as('email')
-    .should('be.visible')
-    .type('daniel@teste.com.br')
-
-    cy.get('#email')
-    .should('have.value', 'daniel@teste.com.br')
-
-    cy.get('#open-text-area')
-    .as('open-text-area')
-    .should('be.visible')
-    .type('teste texto')
-
-    cy.get('#open-text-area')
-    .should('have.value', 'teste texto')
-
-    cy.get('.button').click()
-
-    cy.get('.success')    
-    .should('be.visible')
+    cy.get('.success').should('be.visible')
 
   })
   it('Exercício extra 1 - preenche os campos options', () => {
 
-    cy.wait(2000)
-
-    cy.get('#product')
-    .as('product')
-    .should('be.visible')
-    .type('Cursos')
-
+    const longText = Cypress._.repeat('abcdefghijklmnopqrstuxywz',10)  
+    cy.get('#firstName').as('firstName').should('be.visible').type('Daniel')
+    cy.get('#lastName').as('lastName').should('be.visible').type('Rodrigues')
+    cy.get('#email').as('email').should('be.visible').type('daniel@teste.com.br')
+    cy.get('#open-text-area').as('open-text-area').should('be.visible').type(longText,{delay : 0})
+    cy.get('button[type="submit"]').click()
   })
+
+
+
   it('Exercício extra 2 - exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', () => {
 
-    cy.get('#email')
-    .as('email')
-    .should('be.visible')
-    .type('DanielRodrigues')
+    const longText = Cypress._.repeat('QA',30)  
+    cy.get('#firstName').as('firstName').should('be.visible').type('Daniel')
+    cy.get('#lastName').as('lastName').should('be.visible').type('Rodrigues')
+    cy.get('#email').as('email').should('be.visible').type('daniel@teste.com,br')
+    cy.get('#open-text-area').as('open-text-area').should('be.visible').type(longText,{delay : 0})
+    cy.get('button[type="submit"]').click()
 
-    cy.get('.error').closest('.error')
+    cy.get('.error').should('be.visible')
 
   })
   it('Exercício extra 3 - telefone com valor não-numérico digitado', () => {
-    cy.get('#phone')
-    .as('phone')
-    .should('be.visible')
-    .type('abc')
 
-    cy.get('#phone')
+    cy.get('#phone').as('phone').type('abcde')
+    
     .should('have.value', '')
+
   })
   it('Exercício extra 4 - exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', () => {
 
-    cy.get('#firstName')
-    .as('firstName')
-    .should('be.visible')
-    .type('daniel')
-
-    cy.get('#firstName')
-    .should('have.value', 'daniel')
-
-    cy.get('#lastName')
-    .as('lastName')
-    .should('be.visible')
-    .type('rodrigues')
-
-    cy.get('#lastName')
-    .should('have.value', 'rodrigues')
-
-    cy.get('#email')
-    .as('email')
-    .should('be.visible')
-    .type('daniel@teste.com.br')
-
-    cy.get('#email')
-    .should('have.value', 'daniel@teste.com.br')
-
-    cy.get('#phone-checkbox')
-    .as('phone-checkbox')
-    .check()
-
-    cy.get('#open-text-area')
-    .as('open-text-area')
-    .should('be.visible')
-    .type('teste texto')
-
-    cy.get('#open-text-area')
-    .should('have.value', 'teste texto')
-
-    cy.get('.button').click()
-    cy.get('.error').closest('.error')
+    const longText = Cypress._.repeat('SCRUM',20)  
+    cy.get('#firstName').as('firstName').should('be.visible').type('daniel')
+    cy.get('#lastName').as('lastName').should('be.visible').type('rodrigues')
+    cy.get('#email').as('email').should('be.visible').type('daniel@teste.com.br')
+    cy.get('#phone-checkbox').as('phone-checkbox').click()
+    cy.get('#open-text-area').as('open-text-area').should('be.visible').type(longText,{delay : 0})
+    cy.get('button[type="submit"]').click()
+    
+    cy.get('.error').should('be.visible')
 
   })
   it('Exercício extra 5 - preenche e limpa os campos nome, sobrenome, email e telefone', () => {
 
-    cy.get('#firstName')
-    .as('firstName')
-    .should('be.visible')
-    .type('daniel')
-
-    cy.get('#firstName')
-    .should('have.value', 'daniel')
-
-    cy.get('#lastName')
-    .as('lastName')
-    .should('be.visible')
-    .type('rodrigues')
-
-    cy.get('#lastName')
-    .should('have.value', 'rodrigues')
-
-    cy.get('#email')
-    .as('email')
-    .should('be.visible')
-    .type('daniel@teste.com.br')
-
-    cy.get('#email')
-    .should('have.value', 'daniel@teste.com.br')
-
-    cy.get('#phone')
-    .as('phone')
-    .should('be.visible')
-    .type('999999999')
-
-    cy.get('#phone')
-    .should('have.value', '999999999')
-
-    cy.get('#open-text-area')
-    .as('open-text-area')
-    .should('be.visible')
-    .type('teste texto')
-
-    cy.get('#open-text-area')
-    .should('have.value', 'teste texto')
-
-    cy.get('.button').click()
-
-    cy.wait(3000)
-
-    cy.get('#firstName')
-    .as('firstName')
+    cy.get('#firstName').as('firstName').type('daniel').should('have.value', 'daniel')
     .clear().should('have.value', '')
-
-    cy.get('#lastName')
-    .as('lastName')
+    cy.get('#lastName').as('lastName').type('rodrigues').should('have.value', 'rodrigues')
     .clear().should('have.value', '')
-
-    cy.get('#email')
-    .as('email')
+    cy.get('#email').as('email').type('daniel@teste.com.br').should('have.value', 'daniel@teste.com.br')
     .clear().should('have.value', '')
-
-    cy.get('#phone')
-    .as('phone')
+    cy.get('#phone').as('phone').type('999999999')
     .clear().should('have.value', '')
-
+    cy.get('#open-text-area').as('open-text-area').should('be.visible').type('teste texto')
+    .clear().should('have.value', '')
+    
   })
   it('Exercício extra 6 - exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', () => {
 
-    cy.get('.button').click()
-    cy.get('.error').closest('.error')
+    cy.get('button[type="submit"]').click()
 
-  })
-  it('Exercício extra 8 - outra forma de identificar elementos', () => {
-    cy.contains('Nome')
-    .type('daniel')
-
-    cy.contains('Sobrenome')
-    .type('rodrigues')
-
-    cy.contains('E-mail')
-    .type('daniel@teste.com.br')
-
-    cy.contains('Como podemos te ajudar?')
-    .type('teste texto')
-
-    cy.contains('Enviar').click()
+    cy.get('.error').should('be.visible')
 
   })
   it('Exercício extra 7 - envia o formuário com sucesso usando um comando customizado', () => {
+
+//    const data = {
+//      firstName: 'Angelita',
+//      lastName: 'Jose',
+//      email: 'angela@teste.com.br',
+//      text: 'Valeu.'
+//    }
+
+    cy.fillMandatoryFieldsAndSubmit()
+
+    cy.get('.success').should('be.visible')
+
+  })
+  it('Exercício extra 8 - outra forma de identificar elementos', () => {
+
+    cy.contains('Nome').type('daniel')
+    cy.contains('Sobrenome').type('rodrigues')
+    cy.contains('E-mail').type('daniel@teste.com.br')
+    cy.contains('Como podemos te ajudar?').type('teste texto')
+
+    cy.contains('button','Enviar').click()
 
   })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,12 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('fillMandatoryFieldsAndSubmit', (data = {
+    firstName: 'Jhowe',
+    lastName: 'Silva',
+    email: 'Jhowe@england.com.br',
+    text: 'Thanks.'
+}) =>{
+    cy.get('#firstName').as('firstName').should('be.visible').type(data.firstName)
+    cy.get('#lastName').as('lastName').should('be.visible').type(data.lastName)
+    cy.get('#email').as('email').should('be.visible').type(data.email)
+    cy.get('#open-text-area').as('open-text-area').should('be.visible').type(data.text)
+    cy.get('button[type="submit"]').click()
+})
