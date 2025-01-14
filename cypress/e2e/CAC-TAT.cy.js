a6a50adff5ec68cc5f40b6f6061cf37698ac7f55
danielrodrig86 <daniel.rodrigues1998@live.com>
2025-01-14 19:28:15 -0300

    modificando readme

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -212,7 +212,6 @@ describe('Central de Atendimento ao Cliente TAT', () => {
      })
 
   })
-
   it('Aula 7 - verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', () => {
     cy.contains('a','Política de Privacidade')
       .should('have.attr','href','privacy.html')
@@ -224,5 +223,8 @@ describe('Central de Atendimento ao Cliente TAT', () => {
       .click()
 
     cy.contains('h1','CAC TAT - Política de Privacidade').should('be.visible')
+  })
+  it('Aula 8 - Criar um script no arquivo package.json', () => {
+
   })
 })
